963a0dd86b3f548ab032514f5e55dd10f4ab95f8
GhiyatsNe_ <93048740+ghiyasnedal@users.noreply.github.com>
2025-02-24 21:53:19 +0700

    Add files via upload

---
+++ praktikum1 PW2.php
@@ -0,0 +1,12 @@
+<?php
+
+$nama_depan = "Muhammad" ;
+$nama_belakang = 'Ghiyats' ;
+$umur = 21 ;
+$tb = 171;
+
+
+echo $nama_depan . " " . $nama_belakang;
+echo "<br>nama saya adalah $nama_depan dan saya berumur $umur";
+
+echo "<"
